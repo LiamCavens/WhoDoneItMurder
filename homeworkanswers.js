63d5d35293336bb991751f78b01f0580241b0aa2
Liam Cavens <liamcavens@googlemail.com>
2018-06-04 18:37:11 +0100

    second part of murder

---
+++ homeworkanswers.js
@@ -0,0 +1,8 @@
+console.log("Episode 1 : Bob");
+console.log("Episode 2 : 3");
+console.log("Episode 3 : Ducks, Dogs, Lions");
+console.log(
+  "Episode 4 : Prints all names in a string (Colin will be Sus3 in function), then Upul will be suspect 3 in log outside function"
+);
+console.log("Episode 5 : Just calls info function which name is Poiret");
+console.log("Episode 6 : Outer function = Morty, console log = Rick");
